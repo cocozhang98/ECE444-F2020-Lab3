a4e4599e329160f3e2e0f228a5f2e2133e1ab77a
Coco Zhang <cocoz0312@hotmail.com>
2020-10-02 22:49:18 -0400

    Activity 2 (Chpt 4 modified)

--- hello.py
+++ hello.py
@@ -1,12 +1,36 @@
-from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email, ValidationError, StopValidation
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class validate_email(object):
+    def __init__(self, message=None):
+        self.message = message
+
+    def __call__(self, form, field):
+        if field.data == '':
+            message = "Email cannot be empty."
+            field.errors[:] = []
+            raise StopValidation(message)
+        elif "@" not in field.data:
+            message = field.gettext('Please include a \'@\' in the email address. \''+field.data+'\' is missing an \'@\'.')
+            field.errors[:] = []
+            raise StopValidation(message)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[validate_email()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -19,16 +43,21 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
